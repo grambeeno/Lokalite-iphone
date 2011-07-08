f86f33582c4dfcaaa3a298f8532ca7f5c193acd3
John A. Debay <john@highorderbit.com>
2011-07-08 11:39:37 -0600

    Cleaned up template code.

--- Lokalite/src/featured/FeaturedViewController.m
+++ Lokalite/src/featured/FeaturedViewController.m
@@ -10,34 +10,11 @@
 
 @implementation FeaturedViewController
 
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-}
-*/
+#pragma mark - UIViewController implementation
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)io
 {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc. that aren't in use.
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
+    return io == UIInterfaceOrientationPortrait;
 }
 
 @end
