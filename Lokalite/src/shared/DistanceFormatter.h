0d3874245830cf01ff1fb49d6e1317d7cddcd74f
John A. Debay <john@highorderbit.com>
2011-08-15 14:47:51 -0600

    Fixed a bug in the formatting of distances as strings.

--- Lokalite/src/shared/DistanceFormatter.h
+++ Lokalite/src/shared/DistanceFormatter.h
@@ -9,24 +9,11 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
-typedef double (^DFDistanceConverter)(double);
-
 @interface DistanceFormatter : NSObject
-{
-    NSNumberFormatter * formatter;
-    double threshold;
-    NSString * majorUnit;
-    NSString * minorUnit;
-    DFDistanceConverter distanceConverter;
-}
 
 #pragma mark Instantiation and initialization
 
 + (id)formatter;
-
-#pragma mark Initialization
-
-- (id)init;
 
 #pragma mark Converting distance values to strings
 
--- Lokalite/src/shared/DistanceFormatter.m
+++ Lokalite/src/shared/DistanceFormatter.m
@@ -10,145 +10,56 @@
 #import "Constants.h"
 
 @interface DistanceFormatter ()
-@property (nonatomic, retain) NSNumberFormatter * formatter;
-@property (nonatomic, assign) double threshold;
-@property (nonatomic, copy) NSString * majorUnit;
-@property (nonatomic, copy) NSString * minorUnit;
-@property (nonatomic, copy) DFDistanceConverter distanceConverter;
-
-- (id)initWithMajorUnit:(NSString *)aMajorUnit
-              minorUnit:(NSString *)aMinorUnit
-              threshold:(double)aThreshold
-              converter:(DFDistanceConverter)converter;
+@property (nonatomic, retain) NSNumberFormatter *formatter;
 @end
 
 @implementation DistanceFormatter
 
-@synthesize formatter, threshold, majorUnit, minorUnit;
-@synthesize distanceConverter;
+@synthesize formatter = formatter_;
 
-#pragma mark -
-#pragma mark Memory management
+#pragma mark - Memory management
 
 - (void)dealloc
 {
-    [self setFormatter:nil];
-    [self setMajorUnit:nil];
-    [self setMinorUnit:nil];
-    [self setDistanceConverter:nil];
+    [formatter_ release];
 
     [super dealloc];
 }
 
 #pragma mark -
-#pragma mark Instantiation and initialization
+#pragma mark - Instantiation and initialization
 
 + (id)formatter
 {
-    id obj = nil;
-
-    /*
-    if (setting == kUnitsSettingMetric)
-        obj = [[self alloc] initWithMajorUnit:@"km"
-                                    minorUnit:@"m"
-                                    threshold:1000
-                                    converter:^(double distance) {
-                                          return distance;
-                                      }];
-    else if (setting == kUnitsSettingEnglish)
-     */
-        obj = [[self alloc] initWithMajorUnit:@"mi"
-                                    minorUnit:@"ft"
-                                    threshold:/*5280*/ 0
-                                    converter:^(double distance) {
-                                        return distance * 3.280839895013;
-                                    }];
-
-    return [obj autorelease];
+    return [[[self alloc] init] autorelease];
 }
 
-#pragma mark -
-#pragma mark Initialization
+#pragma mark - Converting distance values to strings
 
-- (id)initWithMajorUnit:(NSString *)aMajorUnit
-              minorUnit:(NSString *)aMinorUnit
-              threshold:(double)aThreshold
-              converter:(DFDistanceConverter)converter
+- (NSString *)distanceAsString:(CLLocationDistance)distanceInMeters
 {
-    if (self = [super init]) {
-        formatter = [[NSNumberFormatter alloc] init];
-        [formatter setFormatterBehavior:NSNumberFormatterBehavior10_4];
-        [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
-        [formatter setLocale:[NSLocale currentLocale]];
+    CLLocationDistance distanceInFeet = distanceInMeters * FEET_PER_METER;
+    CLLocationDistance distanceInMiles = distanceInFeet / FEET_PER_MILE;
+    NSNumber *distance = [NSNumber numberWithDouble:distanceInMiles];
 
-        [self setMajorUnit:aMajorUnit];
-        [self setMinorUnit:aMinorUnit];
-        [self setThreshold:aThreshold];
-        [self setDistanceConverter:converter];
-    }
+    [[self formatter] setMaximumFractionDigits:1];
+    NSString *distanceString = [[self formatter] stringFromNumber:distance];
 
-    return self;
+    return [NSString stringWithFormat:@"%@ mi", distanceString];
 }
 
-- (id)init
-{
-    if (self = [super init]) {
-        formatter = [[NSNumberFormatter alloc] init];
-        [formatter setFormatterBehavior:NSNumberFormatterBehavior10_4];
-        [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
-        [formatter setLocale:[NSLocale currentLocale]];
+#pragma mark - Accessors
 
-        /*
-        if (setting == kUnitsSettingMetric) {
-            [self setThreshold:1000];
-            [self setMajorUnit:@"km"];
-            [self setMinorUnit:@"m"];
-            [self setDistanceConverter:^(double distance) { return distance; }];
-        } else if (setting == kUnitsSettingEnglish) {
-         */
-            [self setThreshold:FEET_PER_MILE];
-            [self setMajorUnit:@"mi"];
-            [self setMinorUnit:@"ft"];
-            [self setDistanceConverter:
-                // convert meters to feet
-                ^(double distance) { return distance * FEET_PER_METER; }];
-        /*
-        }
-         */
+- (NSNumberFormatter *)formatter
+{
+    if (!formatter_) {
+        formatter_ = [[NSNumberFormatter alloc] init];
+        [formatter_ setFormatterBehavior:NSNumberFormatterBehavior10_4];
+        [formatter_ setNumberStyle:NSNumberFormatterDecimalStyle];
+        [formatter_ setLocale:[NSLocale currentLocale]];
     }
 
-    return self;
-}
-
-#pragma mark -
-#pragma mark Converting distance values to strings
-
-- (NSString *)distanceAsString:(CLLocationDistance)originalDistance
-{
-    CLLocationDistance distance = distanceConverter(originalDistance);
-
-    NSString * val = nil;
-    NSString * unit = nil;
-
-    if ([self threshold] > 0 && distance > [self threshold]) {
-        NSInteger major = (NSInteger) distance / [self threshold];
-        NSInteger minor = (NSInteger) distance % (int) [self threshold];
-        NSString * tmp = [NSString stringWithFormat:@"%d.%d", major, minor];
-        double d = [tmp doubleValue];
-        NSNumber * n = [NSNumber numberWithDouble:d];
-
-        [[self formatter] setMaximumFractionDigits:1];
-
-        val = [[self formatter] stringFromNumber:n];
-        unit = [self majorUnit];
-    } else {
-        [formatter setMaximumFractionDigits:0];
-        NSNumber * n = [NSNumber numberWithDouble:distance];
-        val = [[self formatter] stringFromNumber:n];
-        unit = [self minorUnit];
-    }
-
-    return [NSString stringWithFormat:@"%@ %@", val, unit];
+    return formatter_;
 }
 
 @end
