b1ca262ef010b26e91acd176b33471282b2a9390
John A. Debay <john@highorderbit.com>
2011-08-16 10:53:03 -0600

    Implemented trending view controller in terms of the EventStreamViewController.

--- Lokalite/src/trending/TrendingViewController.h
+++ Lokalite/src/trending/TrendingViewController.h
@@ -6,8 +6,8 @@
 //  Copyright 2011 Lokalite. All rights reserved.
 //
 
-#import "LokaliteStreamViewController.h"
+#import "EventStreamViewController.h"
 
-@interface TrendingViewController : LokaliteStreamViewController
+@interface TrendingViewController : EventStreamViewController
 
 @end
--- Lokalite/src/trending/TrendingViewController.m
+++ Lokalite/src/trending/TrendingViewController.m
@@ -8,22 +8,9 @@
 
 #import "TrendingViewController.h"
 
-#import "Event.h"
-#import "Event+GeneralHelpers.h"
-
-#import "EventTableViewCell.h"
-#import "EventDetailsViewController.h"
-
 #import "LokaliteTrendingEventStream.h"
 
-#import "TableViewImageFetcher.h"
-
-#import "LokaliteShared.h"
-#import "SDKAdditions.h"
-
 @interface TrendingViewController ()
-
-@property (nonatomic, retain) TableViewImageFetcher *imageFetcher;
 
 #pragma mark - View initialization
 
@@ -33,17 +20,6 @@
 
 
 @implementation TrendingViewController
-
-@synthesize imageFetcher = imageFetcher_;
-
-#pragma mark - Memory management
-
-- (void)dealloc
-{
-    [imageFetcher_ release];
-    
-    [super dealloc];
-}
 
 #pragma mark - UIViewController implementation
 
@@ -61,115 +37,11 @@
     return NSLocalizedString(@"global.trending", nil);
 }
 
-#pragma mark Configuring the table view
-
-- (void)initializeTableView:(UITableView *)tableView
-{
-    [super initializeTableView:tableView];
-
-    [tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
-    [tableView setRowHeight:[EventTableViewCell cellHeight]];
-}
-
-- (NSString *)reuseIdentifierForIndexPath:(NSIndexPath *)indexPath
-                              inTableView:(UITableView *)tableView
-{
-    return [EventTableViewCell defaultReuseIdentifier];
-}
-
-- (UITableViewCell *)tableViewCellInstanceAtIndexPath:(NSIndexPath *)indexPath
-                                         forTableView:(UITableView *)tableView
-                                      reuseIdentifier:(NSString *)identifier
-{
-    return [EventTableViewCell instanceFromNib];
-}
-
-- (void)configureCell:(EventTableViewCell *)cell
-          inTableView:(UITableView *)tableView
-            forObject:(Event *)event
-{
-    [cell configureCellForEvent:event displayDistance:!![self currentLocation]];
-
-    UIImage *image = [event standardImage];
-    if (!image) {
-        NSURL *url = [NSURL URLWithString:[event standardImageUrl]];
-
-        [[self imageFetcher] fetchImageDataAtUrl:url
-                                       tableView:tableView
-                             dataReceivedHandler:
-         ^(NSData *data) {
-             [event setStandardImageData:data];
-         }
-                            tableViewCellHandler:
-         ^(UIImage *image, UITableViewCell *tvc, NSIndexPath *path) {
-             EventTableViewCell *cell = (EventTableViewCell *) tvc;
-             if ([[cell eventId] isEqualToNumber:[event identifier]])
-                 [[cell eventImageView] setImage:image];
-         }
-                                    errorHandler:
-         ^(NSError *error) {
-             NSLog(@"WARNING: Failed to fetch place image at: %@: %@", url,
-                   error);
-         }];
-    }
-
-    [[cell eventImageView] setImage:image];
-}
-
-- (void)displayDetailsForObject:(Event *)event
-{
-    EventDetailsViewController *controller =
-        [[EventDetailsViewController alloc] initWithEvent:event];
-    [[self navigationController] pushViewController:controller animated:YES];
-    [controller release], controller = nil;
-}
-
-#pragma mark Working with the local data store
-
-- (NSString *)lokaliteObjectEntityName
-{
-    return @"Event";
-}
-
-- (NSPredicate *)dataControllerPredicate
-{
-    NSManagedObjectContext *context = [self context];
-    NSDate *date =
-        [[LokaliteApplicationState currentState:context] dataFreshnessDate];
-    LokaliteDownloadSource *source = [[self lokaliteStream] downloadSource];
-    NSString *sourceName = [source name];
-
-    return [NSPredicate predicateForDownloadSourceName:sourceName
-                                       lastUpdatedDate:date];
-}
-
-- (NSArray *)dataControllerSortDescriptors
-{
-    return [Event dateTableViewSortDescriptors];
-}
+#pragma mark Fetching data from the network
 
 - (LokaliteStream *)lokaliteStreamInstance
 {
     return [LokaliteTrendingEventStream streamWithContext:[self context]];
-}
-
-#pragma mark Fetching data from the network
-
-- (void)processObjectFetchError:(NSError *)error
-                     pageNumber:(NSInteger)pageNumber
-{
-    NSLog(@"%@: processing fetch error for page %d: %@",
-          NSStringFromClass([self class]), pageNumber, error);
-    NSString *title = NSLocalizedString(@"featured.fetch.failed", nil);
-    NSString *message = [error localizedDescription];
-    NSString *dismiss = NSLocalizedString(@"global.dismiss", nil);
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
-                                                    message:message
-                                                   delegate:nil
-                                          cancelButtonTitle:dismiss
-                                          otherButtonTitles:nil];
-    [alert show];
-    [alert release], alert = nil;
 }
 
 #pragma mark - View initialization
@@ -178,16 +50,6 @@
 {
     [[self navigationItem] setLeftBarButtonItem:[self refreshButtonItem]];
     [[self navigationItem] setRightBarButtonItem:[self mapViewButtonItem]];
-}
-
-#pragma mark - Accessors
-
-- (TableViewImageFetcher *)imageFetcher
-{
-    if (!imageFetcher_)
-        imageFetcher_ = [[TableViewImageFetcher alloc] init];
-    
-    return imageFetcher_;
 }
 
 @end
