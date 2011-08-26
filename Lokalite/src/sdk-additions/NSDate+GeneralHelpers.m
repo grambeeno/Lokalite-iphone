04e6bb946249f4439400fe474d1d30a90f625a8f
John A. Debay <john@highorderbit.com>
2011-08-26 14:19:42 -0600

    Removed commented out code.

--- Lokalite/src/sdk-additions/NSDate+GeneralHelpers.m
+++ Lokalite/src/sdk-additions/NSDate+GeneralHelpers.m
@@ -169,20 +169,6 @@
 + (NSString *)textRangeWithStartDate:(NSDate *)startDate
                              endDate:(NSDate *)endDate
 {
-    /*
-    static NSDateFormatter *formatter = nil;
-    if (!formatter) {
-        formatter = [[NSDateFormatter alloc] init];
-        [formatter setLocale:[NSLocale currentLocale]];
-        [formatter setDateStyle:NSDateFormatterNoStyle];
-        [formatter setTimeStyle:NSDateFormatterShortStyle];
-    }
-
-    return [NSString stringWithFormat:@"%@ - %@",
-            [formatter stringFromDate:startDate],
-            [formatter stringFromDate:endDate]];
-     */
-
     NSString *description = nil;
     if ([startDate isOnSameDayAsDate:endDate]) {
         NSString *dayDescription = [startDate descriptionForDay];
